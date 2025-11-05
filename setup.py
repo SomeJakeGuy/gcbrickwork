16136f871dae5595c25f8d222710947d0f70ac6a
SomeJakeGuy <somejakeguy@gmail.com>
2025-11-05 05:39:53 -0500

    Updated Setup.py programming language description.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     url="https://github.com/SomeJakeGuy/gcbrickwork",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
