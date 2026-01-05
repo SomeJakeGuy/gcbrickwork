a4f2a653a042a928c34137c0d4912774d50d6ff9
SomeJakeGuy <39894486+SomeJakeGuy@users.noreply.github.com>
2026-01-05 18:45:27 -0500

    Bump version from 3.0.2 to 3.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="gcbrickwork",
     packages=setuptools.find_packages(),
-    version="3.0.2",
+    version="3.0.3",
     license="MIT",
     author="Some Jake Guy",
     author_email="somejakeguy@gmail.com",
